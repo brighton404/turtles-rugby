79c5009435d218157a557f85736b3adf2aeba67f
brighton404 <brightonjulius1@gmail.com>
2025-04-22 19:19:46 +0300

    1.381

--- frontend/src/pages/news/blog/lib/data.ts
+++ frontend/src/pages/news/blog/lib/data.ts
@@ -1,9 +1,1 @@
-// These are blog categories, integrated in the blog filter function
-import { Category } from "./types";
-export const categories: Category[] = [
-  { id: 1, name: "Rugby" },
-  { id: 2, name: "Tournament" },
-  { id: 3, name: "Boot Camp" },
-  { id: 4, name: "Events"},
-  { id: 5, name: "Age grade"},
-];
+// depreciated
