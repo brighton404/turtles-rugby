f88c805c716f6f73e896daf96688c59e5d58ab2f
brighton404 <brightonjulius1@gmail.com>
2025-05-18 20:13:11 +0300

    1.434

--- frontend/src/pages/about/map.tsx
+++ frontend/src/pages/about/map.tsx
@@ -1,9 +1,17 @@
 import { useEffect } from 'react';
 import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const MapComponent = () => {
   useEffect(() => {
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow,
+    });
+
     const map = L.map('map').setView([-3.2453058, 40.1240178], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,8 +29,7 @@ const MapComponent = () => {
   }, []);
 
   return (
-    <div id="map" style={{ height: '400px', width: '100%' }}
-    ></div>
+    <div id="map" style={{ height: '400px', width: '100%' }}></div>
   );
 };
 
